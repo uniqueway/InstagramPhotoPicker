9ad3f539ffa4c06cf30625cbca774e26d9f59195
Madao <chrishyman256@gmail.com>
2015-11-08 02:29:33 +0800

    edit

--- TWPhotoPicker/TWPhoto.h
+++ TWPhotoPicker/TWPhoto.h
@@ -6,7 +6,7 @@
 @interface TWPhoto : NSObject
 
 @property (nonatomic, readonly) UIImage *thumbnailImage;
-@property (nonatomic, readonly) UIImage *originalImage;
+@property (nonatomic, strong) UIImage *originalImage;
 
 @property (nonatomic, strong) ALAsset *asset;
 
